59a9eb8afd0ea0c7be389aa0d322b658b48fc120
unknown <withbbang@gmail.com>
2022-01-13 17:11:29 +0900

    first commit

---
+++ blockChain.js
@@ -0,0 +1,90 @@
+const sha256 = require("sha256");
+
+//클래스를 이용한 블록체인 데이터 구조 위와 동일하다...
+class BlockChain {
+  constructor() {
+    this.chain = [];
+    this.pendingTransaction = [];
+    //제네시스 블락 - 임의의 인자값 넣어준다.
+    this.createNewBlock(100, "0", "0");
+  }
+}
+
+//블록체인 프로토 타입 함수 정의
+BlockChain.prototype.createNewBlock = function (
+  nonce,
+  previousBlockHash,
+  hash,
+) {
+  //새 블록 객체
+  const newBlock = {
+    index: this.chain.length + 1,
+    timestamp: Date.now(),
+    transactions: this.pendingTransaction,
+    nonce: nonce,
+    hash: hash,
+    previousBlockHash: previousBlockHash,
+  };
+
+  //다음 거래를 위한 거래내역 배열 비워주고 새로운 블록을 chin 배열에 추가
+  this.pendingTransaction = [];
+  this.chain.push(newBlock);
+
+  return newBlock;
+};
+
+//마지막 블록 얻기 - chain 배열에는 블록데이터가 들어간다. 맨마지막 블록을 가져와라.
+BlockChain.prototype.getLastBlock = function () {
+  return this.chain[this.chain.length - 1];
+};
+
+//새로운 트랜젝션(거래)가 발생했을 때 작동되는 함수
+//인자값으로, 총액수, 보내는사람, 받는사람이 들어간다.
+BlockChain.prototype.createNewTransaction = function (
+  amount,
+  sender,
+  recipient,
+) {
+  const newTransaction = {
+    amount: amount,
+    sender: sender,
+    recipient: recipient,
+  };
+
+  //맨위 트랜잭션 배열에 값을 넣어준다.
+  this.pendingTransaction.push(newTransaction);
+
+  //마지막 블록의 index 에서 + 1
+  return this.getLastBlock()["index"] + 1;
+};
+
+//해쉬 값 리턴 함수
+BlockChain.prototype.hashBlock = function (
+  previousBlockHash,
+  currentBlockData,
+  nonce,
+) {
+  const dataAsString =
+    previousBlockHash + nonce.toString() + JSON.stringify(currentBlockData);
+  const hash = sha256(dataAsString);
+  return hash;
+};
+
+//pow 작업 함수 - 이전블록의 해쉬, 현재 블록 데이터와 nonce 값을 사용한다.
+BlockChain.prototype.proofOfWork = function (
+  previousBlockHash,
+  currentBlockData,
+) {
+  let nonce = 0;
+  let hash = this.hashBlock(previousBlockHash, currentBlockData, nonce);
+  while (hash.substring(0, 4) != "0000") {
+    nonce++;
+    hash = this.hashBlock(previousBlockHash, currentBlockData, nonce);
+  }
+  return nonce;
+};
+
+출처: //abc1211.tistory.com/522?category=1003529 [길위의 개발자]
+
+//BlockChain 모듈화
+https: module.exports = BlockChain;
---
+++ index.js
@@ -0,0 +1,90 @@
+const express = require("express");
+const app = express();
+const PORT = 3000;
+const bodyParser = require("body-parser");
+
+const BlockChain = require("./blockChain");
+const bitcoin = new BlockChain();
+
+//나의 네트워크 고유 아이디 생성
+const { v1 } = require("uuid");
+const nodeAddress = v1().split("-").join("");
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+
+app.get("/", function (req, res) {
+  res.send("Hello World");
+});
+
+// 웹브라우저에 get 방식으로 /blockchain 주소를 입력했을 때 실행
+app.get("/blockchain", function (req, res) {
+  res.send(bitcoin);
+});
+
+// 웹브라우저에 post 방식으로 /transaction 주소를 입력했을 때 실행
+app.post("/transaction", function (req, res) {
+  const blockIndex = bitcoin.createNewTransaction(
+    req.body.amount,
+    req.body.sender,
+    req.body.recipient,
+  );
+  res.json({ note: `트랜젝션은 ${blockIndex} 블락안으로 들어갈 예정입니다.` });
+});
+
+// 웹브라우저에 get 방식으로 /mine 주소를 입력했을 때 실행
+app.get("/mine", function (req, res) {
+  //마지막 블럭을 가져온다.
+  const lastBlock = bitcoin.getLastBlock();
+
+  //마지막 블럭의 해쉬 값, 즉 이전 블럭의 해쉬값
+  const previousBlockHash = lastBlock["hash"];
+
+  //현재 블락의 데이터 : 미완료된 거래내역 + 블락의 index 값
+  const currentBlockData = {
+    transactions: bitcoin.pendingTransaction,
+    index: lastBlock["index"] + 1,
+  };
+
+  //이전블락해쉬, 현재블럭 데이터를 proofOfWork에 넣고 맞는 hash값(0000sfaff...)을 찾고 해당 nonce 값을 리턴.
+  const nonce = bitcoin.proofOfWork(previousBlockHash, currentBlockData);
+  //이전블락해쉬, 현재블럭 데이터, nonce 값을 넣고 현재 블락의 해쉬 값 리턴
+  const blockHash = bitcoin.hashBlock(
+    previousBlockHash,
+    currentBlockData,
+    nonce,
+  );
+
+  //채굴에 대한 보상
+  bitcoin.createNewTransaction(10, "bosang0000", nodeAddress);
+
+  //새로운 블락을 생성하려면 nonce,previousBlockHash,blockHash 값이 필요하다.
+  const newBlock = bitcoin.createNewBlock(nonce, previousBlockHash, blockHash);
+
+  res.json({
+    note: "새로운 블락이 성공적으로 만들어졌습니다.",
+    newBlock: newBlock,
+  });
+});
+
+app.listen(PORT, () => {
+  console.log(`server running on ${PORT || 3000}`);
+});
+
+// //blockChain.js 모듈을 이곳에서 가져다 쓰겠다.
+// const BlockChain = require("./blockChain");
+
+// //위에서 가져온 모듈의 객체를 만든다.
+// const bitcoin = new BlockChain();
+
+// //새로운 블락 만들기
+// bitcoin.createNewBlock(1111, "aaaaaaa", "1a1a1a1a1a1a");
+
+// //새로은 트랜잭션 생성 - (총금액, 보내는이, 받는이)
+// bitcoin.createNewTransaction(100, "PACKadffaaf", "HONGllalflks");
+
+// //새로운 블락 생성 - 채굴후 얻은 블락
+// bitcoin.createNewBlock(2222, "bbbbbbb", "2b2b2b2b2b2b");
+
+// //찍어보기
+// console.log(bitcoin);
