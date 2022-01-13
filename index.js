d7ff8ae1902af0871b5bffc3d99dd4ef8609a589
unknown <withbbang@gmail.com>
2022-01-13 17:14:23 +0900

    second commit

--- index.js
+++ index.js
@@ -70,21 +70,3 @@ app.get("/mine", function (req, res) {
 app.listen(PORT, () => {
   console.log(`server running on ${PORT || 3000}`);
 });
-
-// //blockChain.js 모듈을 이곳에서 가져다 쓰겠다.
-// const BlockChain = require("./blockChain");
-
-// //위에서 가져온 모듈의 객체를 만든다.
-// const bitcoin = new BlockChain();
-
-// //새로운 블락 만들기
-// bitcoin.createNewBlock(1111, "aaaaaaa", "1a1a1a1a1a1a");
-
-// //새로은 트랜잭션 생성 - (총금액, 보내는이, 받는이)
-// bitcoin.createNewTransaction(100, "PACKadffaaf", "HONGllalflks");
-
-// //새로운 블락 생성 - 채굴후 얻은 블락
-// bitcoin.createNewBlock(2222, "bbbbbbb", "2b2b2b2b2b2b");
-
-// //찍어보기
-// console.log(bitcoin);
